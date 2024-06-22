4f64282df3c79ff249b30fb2ab13a051efcceba3
AndrMaulana <andryymaulana2000@gmail.com>
2024-06-22 16:28:53 +0700

    list entity

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,27 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	ID               int
+	UserID           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerAmount     int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+}
+
+type CampaignImages struct {
+	ID         int
+	CampaignID int
+	FileName   string
+	IsPrimary  int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
