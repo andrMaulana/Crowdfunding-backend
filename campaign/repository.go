e18edc23beaf7f6ba8e237be4b16dc47f0be0f26
AndrMaulana <andryymaulana2000@gmail.com>
2024-06-23 23:18:20 +0700

    buat kontrak campaign repository

--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,16 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	FindAll() ([]Campaign, error)
+	FindByID(userID int) ([]Campaign, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
