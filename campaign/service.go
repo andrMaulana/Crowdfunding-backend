95c3760da1ddf624ba0408a0da98a5275654540f
AndrMaulana <andryymaulana2000@gmail.com>
2024-06-29 20:39:54 +0700

    create generate slug✨

--- campaign/service.go
+++ campaign/service.go
@@ -1,5 +1,11 @@
 package campaign
 
+import (
+	"fmt"
+
+	"github.com/gosimple/slug"
+)
+
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
@@ -50,7 +56,8 @@ func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
 	campaign.GoalAmount = input.GoalAmount
 	campaign.UserID = input.User.ID
 
-	// proses pembuatan slug
+	generateSlug := fmt.Sprintf("%s %s", input.Name, input.User.ID)
+	campaign.Slug = slug.Make(generateSlug)
 
 	newCampaign, err := s.repository.Save(campaign)
 	if err != nil {
