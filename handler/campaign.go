869d460af27b2f2521ca128b19e7fb195680e965
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-03 22:21:51 +0700

    set validation input upload images campaign🚀

--- handler/campaign.go
+++ handler/campaign.go
@@ -123,14 +123,13 @@ func (h *campaignHandler) UploadImage(c *gin.Context) {
 
 	err := c.ShouldBind(&input)
 	if err != nil {
-		response := helper.APIResponse("Failed to upload campaign image", http.StatusBadRequest, "error", nil)
-		c.JSON(http.StatusBadRequest, response)
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed to update campaign", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
-
-	currentUser := c.MustGet("currentUser").(user.User)
-	input.User = currentUser
-	userID := currentUser.ID
 
 	file, err := c.FormFile("file")
 	if err != nil {
