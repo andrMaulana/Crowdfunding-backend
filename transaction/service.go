834e31eee79debbe74ac1ea02099808fec56619f
AndrMaulana <andryymaulana2000@gmail.com>
2024-07-05 17:16:53 +0700

    add service transaction🚀

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,22 @@
 package transaction
+
+type Service interface {
+	GetTransactionByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetTransactionByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+	transactions, err := s.repository.GetByCampaignID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
